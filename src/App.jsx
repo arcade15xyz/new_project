a92d9ac8830532143b0dbc4ed2151102e5c3bc41
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2024-12-25 12:30:29 +0530

    Now on clicking button NewProject Opens up

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
-import './App.css'
-import ProjectsSidebar from './components/ProjectsSidebar'
-import NewProject from './components/NewProject'
+import {useState} from 'react';
+
+import './App.css';
+import ProjectsSidebar from './components/ProjectsSidebar';
+import NewProject from './components/NewProject';
 import NoProjectSelected from './components/NoProjectSelected';
 
 export default function App() {
-  return (
+  const [projectsState,setProjectsState] = useState(
+    {
+      selectedProjectId: undefined,
+      projects:[]
+    }
+  );
+  function handleStartAddProject(){
+    setProjectsState(prevState =>{
+      return{
+        ...prevState,
+        selectedProjectId: null,
+    };
+    }
+  );
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId ===null){
+    content = <NewProject/>;
+  }
+
+  else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
+
+  return(
     <>
       <main className=' h-screen my-8 flex gap-8'>
-        <ProjectsSidebar></ProjectsSidebar>
-        <NoProjectSelected></NoProjectSelected>
-        {/* <NewProject></NewProject> */}
+        <ProjectsSidebar onStartAddProject={handleStartAddProject}></ProjectsSidebar>
+        {content}
       </main>
         
     </>
-  );
+  )
+
+  
 }
 
