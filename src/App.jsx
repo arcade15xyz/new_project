d8dbf21c3b95141b8fec1293a47b7cc39189a8e8
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2024-12-25 11:34:03 +0530

    Added Tailwind css classes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <main className=' h-screen my-8'>
+      <main className=' h-screen my-8 flex gap-8'>
         <ProjectsSidebar></ProjectsSidebar>
         <NewProject></NewProject>
       </main>
