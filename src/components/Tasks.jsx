9f89814a3f99f1ad1123b2035214763ac8d9e9e3
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2024-12-27 11:54:55 +0530

    completed

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,7 +6,7 @@ export default function Tasks({tasks,onAdd,onDelete}){
         <NewTask onAdd={onAdd}/>
         {tasks.length ===0 &&(<p>This Project does not have any tasks yet.</p>)}
         {tasks.length>0 &&(<ul className=" p-4 mt-8 rounded-md bg-stone-100">
-             {tasks.map((task)=><li key={task.id} className=" flex justify-between my-4"><span>{task.text}</span><button className="text-stone-700 hover:text-red-500">Clear</button></li>)}
+             {tasks.map((task)=><li key={task.id} className=" flex justify-between my-4"><span>{task.text}</span><button className="text-stone-700 hover:text-red-500" onClick={()=>onDelete(task.id)}>Clear</button></li>)}
         </ul>)}
     </section>
 }
